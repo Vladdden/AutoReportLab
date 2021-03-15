e40ea178f2bb5f376fe571985ce45b7df4760170
Vladislav <Vlad2999009@yandex.ru>
2021-03-16 02:48:18 +0300

    небольшие правки

--- AutoReportLab/AutoReportLab/Program.cs
+++ AutoReportLab/AutoReportLab/Program.cs
@@ -4,10 +4,45 @@ namespace AutoReportLab
 {
     internal class Program
     {
+        private static Workers workers;
+        public Program()
+        {
+            workers = new Workers();
+        }
         public static void Main(string[] args)
         {
-            Workers workers = new Workers();
-            Console.WriteLine(workers.workerStatus);
+            
+            bool isExit = false;
+            switch (workers.workerStatus)
+            {
+                case 100:
+                    while (!isExit)
+                        isExit = UserIsTeamLeader();
+                    break;
+                case 10:
+                    while (!isExit)
+                        isExit = UserIsBoss();
+                    break;
+                case 1:
+                    while (!isExit)
+                        isExit = UserIsBoss();
+                    break;
+            }
+        }
+
+        public static bool UserIsTeamLeader()
+        {
+            
+        }
+        
+        public static bool UserIsBoss()
+        {
+            
+        }
+        
+        public static bool UserIsWorker()
+        {
+            
         }
     }
 }
